408eed3b9263865cdc48caa2f64a60188b84390d
Elisa JF <elisajf2001@gmail.com>
2021-03-08 15:53:24 +0100

    Fix function names in test

--- analysr/tests/testthat/test-import_period.R
+++ analysr/tests/testthat/test-import_period.R
@@ -1,7 +1,7 @@
 library(dplyr) # pour réaliser le test nécessaire: https://community.rstudio.com/t/all-equal-on-tibbles-ignores-attributes/4299/2
 
 test_that("import period CSV works", {
-  result <- import_period_CSV("./set-import-period.csv")
+  result <- import_period_csv("./set-import-period.csv")
 
   stat_unit <- c(101929077, 101929076)
   begin <- c(lubridate::ymd_hm("2006-10-23 12:00"), lubridate::ymd_hm("2006-11-10 11:00"))
@@ -17,7 +17,7 @@ test_that("import period CSV works", {
 
 
 test_that("import period CSV 2 works", {
-  result <- import_period_CSV_2("./set-import-period-2.csv", "PERSON", "START", "END")
+  result <- import_period_csv_2("./set-import-period-2.csv", "PERSON", "START", "END")
 
   stat_unit <- c(101929077, 101929076)
   begin <- c(lubridate::ymd_hm("2006-10-23 12:00"), lubridate::ymd_hm("2006-11-10 11:00"))
