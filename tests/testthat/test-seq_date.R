b63f24ffeb5f94e272481655b6e3c38a4c506ac5
Cédric JUNG <cedced19@gmail.com>
2021-03-21 10:46:28 +0100

    Put all project in first folder

--- tests/testthat/test-seq_date.R
+++ tests/testthat/test-seq_date.R
@@ -2,7 +2,6 @@ test_that("seq_date works", {
   begin <- "1900 01 01"
   end <- "1900 03 24"
   frequency <- 12*lubridate::days()
-
   excepted_days <- lubridate::as_date(c("1900 01 01", "1900 01 13", "1900 01 25",
                                         "1900 02 06", "1900 02 18", "1900 03 02",
                                         "1900 03 14"))
