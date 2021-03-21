8565f7c2d1ebc33c18e450d4abd11050698fada7
manuhuer <manuhuer@hotmail.com>
2021-03-21 21:05:42 +0100

    fichier avec imputmeasure plus création environement

---
+++ environement_measure.R
@@ -0,0 +1,16 @@
+elocal <- new.env()
+
+
+import_measures_csv <- function (csv_path, id = "patient", date = "date_prlvt", tag = "type_examen", value = "valeur"){
+  
+  
+  result <- readr::read_csv(file=csv_path)
+  result <- as.data.frame(result)
+  
+  result <- result[c(id,date,tag,value)]
+  # https://stackoverflow.com/questions/10085806/extracting-specific-columns-from-a-data-frame
+  # on pourrait utiliser dplyr pour extraire des colonnes
+  colnames(result) <- c("id", "date", "tag", "value")
+  
+  elocal$measures <- result
+}
