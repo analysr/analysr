4eda12f406c828e7d7c81590e1b0634ade819f42
Elisa JF <elisajf2001@gmail.com>
2021-04-11 10:20:31 +0200

    Keep working on impute and test_impute (problem with dates comparisons in the creation of SAMPLE)

--- R/impute.R
+++ R/impute.R
@@ -13,37 +13,50 @@ impute <-
     data <- subset (data, tag = tag_wanted)
     data <- subset (data, date > period_start)
     data <- subset (data, date < period_end)
-    data <- subset (data, stat_unit = stat_unit_wanted)
+    data <- subset (data, stat_unit == stat_unit_wanted)#ok
+
 
 # let's initialize our dataframe
     final_date <- seq_date(period_start, period_end, temporal_granularity)
-    n <- length(dates)
+    n <- length(final_date)
     final_tag <- rep(tag_wanted, n)
     final_stat_unit <- rep(stat_unit_wanted, n)
     final_value <- 1:n
     status <- rep("NOT TREATED", n)
 
     result <- data.frame(final_stat_unit, final_date,
-                         final_tag, final_value, status)
+                         final_tag, final_value, status)#ok
+    print(n)
 
 # we now have to fill the value column
 
     #let's start by aggregating the values we do have
     for (i in 1:n){ # i is the index in result
-      sample <- subset(data, date > result$final_date[i])
+
+      #in sample are all the values taken in the interval of time that we're
+      #working on (for example a day)(between the i date and the (i+1))
+
+      sample <- subset(data, lubridate::ymd_hm(date) >= result$final_date[i])
+      print(result$final_date[i])
       sample <- subset(
         data, date < (result$final_date[i] + temporal_granularity))
+      print(i)
+      print(sample)
+
       if (length(sample$date) > 0){
-        result$final_value[i] <- aggregate(sample,
-                                     result$final_date[i],
-                                     result$final_date[i] +
-                                       temporal_granularity,
-                                     aggregation_method)
+        #result$final_value[i] <- aggregate(sample,
+                                     #result$final_date[i],
+                                     #result$final_date[i] +
+                                      # temporal_granularity,
+                                    # aggregation_method)
+        result$final_value[i] <- aggregation_method(sample$value)
         result$status[i] <- "AGGREGATED"
       }
     }
+
     #let's complete by imputing the missing values
     for (i in 1:n){
+
       if ( result$status[i] == "NOT TREATED" ) {
         result$status[i] <- "IMPUTED"
         j <- 1
@@ -57,5 +70,6 @@ impute <-
 
 
     }
+    result
 
     }
--- tests/testthat/test-impute.R
+++ tests/testthat/test-impute.R
@@ -1,3 +1,32 @@
 test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+
+  stat_unit <- c(101929076, 101929077, 101929077, rep(101929077, 9))
+  date <- lubridate::ymd_hm(c("06-11-10 11:00", "06-10-23 12:00", "06-11-10 12:00",
+                              "06-11-11 12:00", "06-11-11 20:00", "06-11-13 11:00",
+                              "06-11-16 11:00", "06-11-16 13:00", "06-11-16 15:00",
+                              "06-11-16 16:00", "06-11-17 11:00", "06-11-17 13:00"))
+  tag <- c("Kaliemie", "Cholesterol", rep("Kaliemie", 10))
+  value <- c(4.7, 0.4, 5.1, 7.4, 6.8, 5.3, 8.2, 7.9, 8.3, 8.5, 7.4, 5.8)
+  d <- data.frame(stat_unit, date, tag, value)
+
+  result <- impute(data = d,
+                   tag_wanted = "Kaliemie",
+                   period_start = lubridate::ymd_hm("06-11-10 10:00"),
+                   period_end = lubridate::ymd_hm("06-11-17 10:00"),
+                   temporal_granularity = lubridate::days(),
+                   stat_unit_wanted = 101929076)
+
+  stat_unit <- rep(101929076, 8)
+  date <- lubridate::ymd_hm("06-11-10 10:00") + lubridate::days() * c(0:7)
+  tag <- rep("Kaliemie", 8)
+  value <- ????
+  status <- ????
+
+  expected_result <-
+
+
+  expect_equal(result, expected_result)
+
+
+
 })
