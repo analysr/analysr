499ca7e73eaf76516d3e850a1212cefcf4dc357b
Cédric JUNG <cedced19@gmail.com>
2021-04-17 16:42:36 +0200

    Add stat_units when importing events

--- R/import_events_csv.R
+++ R/import_events_csv.R
@@ -9,10 +9,13 @@ import_events_csv <-
     result <- quiet_read_csv(file = csv_path)$result
     result <- as.data.frame(result)
 
+    
+
     result <- result[c(stat_unit, date, tag)]
-    # https://stackoverflow.com/questions/10085806/extracting-specific-columns-from-a-data-frame
-    # on pourrait utiliser dplyr pour extraire des colonnes
+    # we could use dplyr to extract colums https://bit.ly/32lGkNR
     colnames(result) <- c("stat_unit", "date", "tag")
+
+    add_stat_units(result$stat_unit)
 
     result <- cbind(
       hash = get_hash(nrow(result)),
