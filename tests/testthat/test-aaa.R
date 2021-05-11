c222f0e80431380bf8b37ab39fc93e1e1adcc7f2
mcaron01 <mathcaron_25@yahoo.fr>
2021-05-11 11:47:32 +0200

    add test get_hash

--- tests/testthat/test-aaa.R
+++ tests/testthat/test-aaa.R
@@ -59,3 +59,19 @@ test_that("setup_new_env works", {
   )
 
 })
+
+test_that("get_hash works", {
+
+  setup_new_env()
+
+  expect_equal(
+    get_hash(1),1
+  )
+
+  setup_new_env()
+
+  expect_equal(
+    get_hash(15),1:15
+  )
+
+})
