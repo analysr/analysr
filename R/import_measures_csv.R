dae740401bf872b7f20acc34cdfef4ef2ea2fd5b
Cédric JUNG <cedced19@gmail.com>
2021-05-13 20:17:30 +0200

    Add values to match dates

--- R/import_measures_csv.R
+++ R/import_measures_csv.R
@@ -11,8 +11,10 @@
 #' @param tag A string containing the tag label.
 #' @param value A string containing the value label.
 #' @param optional_data A vector containing label to import in descriptions
-#' table.
+#' table (not required).
 #' @param status A string containing the status label.
+#' @param date_format_func A function to format date with (not required).
+#' @param date_format_reg A expression to format date with (not required).
 #'
 #' @export
 import_measures_csv <-
@@ -22,7 +24,9 @@ import_measures_csv <-
             tag = "tag",
             value = "value",
             optional_data,
-            status = "status") {
+            status = "status",
+            date_format_func = lubridate::ymd_hms,
+            date_format_reg) {
     quiet_read_csv <- purrr::quietly(readr::read_csv)
 
     result <- quiet_read_csv(file = csv_path)$result
@@ -31,7 +35,7 @@ import_measures_csv <-
     n <- nrow(result)
     hash <- get_hash(n)
 
-    if (!("status" %in% colnames(result))) {
+    if (!(status %in% colnames(result))) {
       result <- cbind(
         result,
         status = rep("", n)
@@ -40,6 +44,14 @@ import_measures_csv <-
 
     if (!missing(optional_data)) {
       fill_descriptions(hash, optional_data, result,n)
+    }
+
+    if (missing(date_format_reg)) {
+      # use function passed in argument (or default function)
+      result$date <- date_format_func(result$date)
+    } else {
+      # use format passed in argument (or default)
+      result$date <- lubridate::parse_date_time2(result$date, date_format_reg)
     }
 
     result <- result[c(stat_unit, date, tag, value, status)]
