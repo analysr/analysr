27f4693bd1f3726f363d1073043b341532a40fd9
Elisa JF <elisajf2001@gmail.com>
2021-05-14 18:30:01 +0200

    Add a test with different date format for fix_granularity

--- tests/testthat/test-fix_granularity.R
+++ tests/testthat/test-fix_granularity.R
@@ -1,5 +1,37 @@
 test_that("fix_granularity works", {
 
+  ### FIRST TEST ###
+
+  setup_new_env()
+
+  expected_result <- import_measures_csv(
+    csv_path = "./csv/fix_granularity_csv/test_fix_granularity_after1.csv",
+    date_format_reg = "ymd-HM")
+
+  # using readr may be better
+
+  setup_new_env()
+  import_measures_csv(
+    csv_path = "./csv/fix_granularity_csv/test_fix_granularity_before1.csv",
+    date_format_reg = "ymd-HM")
+
+
+  result <- fix_granularity(
+    tag_wanted = "Kaliemie",
+    period_start = lubridate::ymd_hm("06-11-10 10:00"),
+    period_end = lubridate::ymd_hm("06-11-17 10:00"),
+    stat_unit_wanted = 101929076,
+    temporal_granularity = lubridate::days())
+
+  print(result)
+  print(expected_result)
+
+  expect_equal(result[c("stat_unit", "date", "tag", "value", "status")],
+               expected_result[c("stat_unit", "date", "tag", "value", "status")])
+
+
+  ### SECOND TEST ###
+
   setup_new_env()
 
   expected_result2 <- import_measures_csv(
