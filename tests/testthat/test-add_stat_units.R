3e2a7f7a9605b90c7d2fa18f1b36ede96c4b8f54
Cédric JUNG <cedced19@gmail.com>
2021-05-19 17:03:58 +0200

    Add test to better check that add_stat_units works

--- tests/testthat/test-add_stat_units.R
+++ tests/testthat/test-add_stat_units.R
@@ -4,12 +4,14 @@ test_that("add_stat_units works", {
   # add some elements
   add_stat_units(c(144545, 78787, 845, 144545))
 
-  # Check that stat_units number is 3
-  expect_equal(nrow(analysr_env$stat_units), 3)
+  expected <- data.frame(hash = c(1L, 2L, 3L),
+                         stat_unit = c(144545, 78787, 845))
+
+  expect_equal(dplyr::all_equal(expected, analysr_env$stat_units), TRUE)
 
   # try to add a element
   add_stat_units(c(845))
 
-  # Check that stat_units number is still 3
-  expect_equal(nrow(analysr_env$stat_units), 3)
+  expect_equal(dplyr::all_equal(expected, analysr_env$stat_units), TRUE)
+
 })
