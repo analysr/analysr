9552d2475966a44a7ed10f87206ca0d78514f957
Cédric JUNG <cedced19@gmail.com>
2021-05-26 17:01:41 +0200

    Change test for fix_granularity

--- tests/testthat/test-fix_granularity.R
+++ tests/testthat/test-fix_granularity.R
@@ -1,20 +1,8 @@
-test_that("fix_granularity works", {
-
-  ### FIRST TEST ###
+test_that("fix_granularity works (test 1)", {
 
   setup_new_env()
 
-  expected_result <- import_measures_csv(
-    csv_path = "./csv/fix_granularity_csv/test_fix_granularity_after1.csv",
-    date_format_reg = "ymd-HM")
-
-  # using readr may be better
-
-  setup_new_env()
-  import_measures_csv(
-    csv_path = "./csv/fix_granularity_csv/test_fix_granularity_before1.csv",
-    date_format_reg = "ymd-HM")
-
+  load_env_csv("./csv/fix_granularity_csv/before1")
 
   fix_granularity(
     tag_wanted = "Kaliemie",
@@ -23,23 +11,13 @@ test_that("fix_granularity works", {
     stat_unit_wanted = c(101929076, 101929077),
     temporal_granularity = lubridate::days())
 
-  expect_equal(analysr_env$measures[c("stat_unit", "date", "tag", "value", "status")],
-               expected_result[c("stat_unit", "date", "tag", "value", "status")])
+  expect_equal(model_state_equal("./csv/fix_granularity_csv/after1"), TRUE)
 
-
-  ### SECOND TEST ###
-
+})
+test_that("fix_granularity works (test 2)", {
   setup_new_env()
 
-  expected_result2 <- import_measures_csv(
-    csv_path = "./csv/fix_granularity_csv/test_temperature_after.csv")
-
-  # using readr may be better
-
-  setup_new_env()
-  import_measures_csv(
-    csv_path = "./csv/fix_granularity_csv/test_temperature_before.csv")
-
+  load_env_csv("./csv/fix_granularity_csv/before2")
 
   fix_granularity(
     tag_wanted = "Temperature",
@@ -48,24 +26,12 @@ test_that("fix_granularity works", {
     stat_unit_wanted = c(108),
     temporal_granularity = lubridate::hours())
 
-  expect_equal(analysr_env$measures[c("stat_unit", "date", "tag", "value", "status")],
-      expected_result2[c("stat_unit", "date", "tag", "value", "status")])
-
-
-
-  ### THIRD TEST ###
-  ## Test if the gaps are well treated
+  expect_equal(model_state_equal("./csv/fix_granularity_csv/after2"), TRUE)
+})
+test_that("fix_granularity works: test if the gaps are well treated", {
   setup_new_env()
 
-  expected_result3 <- import_measures_csv(
-      csv_path = "./csv/fix_granularity_csv/test_temperature_gap_after.csv")
-
-  # using readr may be better
-
-  setup_new_env()
-  import_measures_csv(
-    csv_path = "./csv/fix_granularity_csv/test_temperature_gap_before.csv")
-
+  load_env_csv("./csv/fix_granularity_csv/before3")
 
   fix_granularity(
     tag_wanted = "Temperature",
@@ -74,23 +40,13 @@ test_that("fix_granularity works", {
     stat_unit_wanted = c(108),
     temporal_granularity = lubridate::hours())
 
-  expect_equal(analysr_env$measures[c("stat_unit", "date", "tag", "value", "status")],
-               expected_result3[c("stat_unit", "date", "tag", "value", "status")])
-
-
-  ### FOURTH TEST ###
-  ## Test if the gaps are well treated (one gap in the middle and one at the end)
+  expect_equal(model_state_equal("./csv/fix_granularity_csv/after3"), TRUE)
+})
+test_that("fix_granularity works: test if the gaps are well treated
+          (one gap in the middle and one at the end)", {
   setup_new_env()
 
-  expected_result4 <- import_measures_csv(
-    csv_path = "./csv/fix_granularity_csv/test_temperature_gap_after.csv")
-
-  # using readr may be better
-
-  setup_new_env()
-  import_measures_csv(
-    csv_path = "./csv/fix_granularity_csv/test_temperature_gap_before.csv")
-
+  load_env_csv("./csv/fix_granularity_csv/before4")
 
   fix_granularity(
     tag_wanted = "Temperature",
@@ -98,20 +54,14 @@ test_that("fix_granularity works", {
     period_end = lubridate::ymd_hms("10-03-13 10:00:00"),
     stat_unit_wanted = c(108),
     temporal_granularity = lubridate::hours())
-  expect_equal(analysr_env$measures[c("stat_unit", "date", "tag", "value", "status")],
-               expected_result4[c("stat_unit", "date", "tag", "value", "status")])
 
-  ### FIFTH TEST ###
-  ## Test if the gaps are well treated (one gap in the middle and one at the beginning)
+  expect_equal(model_state_equal("./csv/fix_granularity_csv/after4"), TRUE)
+})
+test_that("fix_granularity works: test if the gaps are well treated
+          (one gap in the middle and one at the beginning)", {
   setup_new_env()
 
-  expected_result5 <- import_measures_csv(
-    csv_path = "./csv/fix_granularity_csv/test_temperature_gap_after.csv")
-
-  setup_new_env()
-  import_measures_csv(
-    csv_path = "./csv/fix_granularity_csv/test_temperature_gap_before.csv")
-
+  load_env_csv("./csv/fix_granularity_csv/before5")
 
   fix_granularity(
     tag_wanted = "Temperature",
@@ -120,6 +70,5 @@ test_that("fix_granularity works", {
     stat_unit_wanted = c(108),
     temporal_granularity = lubridate::hours())
 
-  expect_equal(analysr_env$measures[c("stat_unit", "date", "tag", "value", "status")],
-               expected_result5[c("stat_unit", "date", "tag", "value", "status")])
+  expect_equal(model_state_equal("./csv/fix_granularity_csv/after5"), TRUE)
 })
