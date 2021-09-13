bc615ca66e219115131effd88c2f99b3b64f3413
Cédric JUNG <cedced19@gmail.com>
2021-09-13 09:59:22 +0200

    Add before Need to fix "within"

--- R/before.R
+++ R/before.R
@@ -1,3 +1,32 @@
-before <- function(model, event) {
+is_before <- function(entry, duration, events) {
+  # check if (entry date) =< (event date) and (entry date) >= (event date + duration)
+  found <- FALSE
+  print(entry)
+  for (i in rownames(events)) {
+    end <- events[i,]$date - duration
+    if (entry$date %within% list(events[i,]$date, end)) {
+      break
+    }
+  }
+  found
+}
 
+is_before_list <- function(entries, duration, events) {
+  status <- c()
+  for (i in rownames(entries)) {
+    status <- c(status, is_before(entries[i,], duration, events))
+  }
+  status
+}
+
+before <- function(model, event) {
+  nmodel <- model
+  # TODO: check if at_most and tag flag exist
+
+  duration <- nmodel$query$at_most
+  events <- subset(nmodel$events, tag == event)
+
+  nmodel$measures <- nmodel$measures[is_before_list(nmodel$measures, duration, events)]
+
+  nmodel
 }
