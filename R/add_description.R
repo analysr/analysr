3ea1ef6e6667dc80155eba3628fe23ca1e068c65
Cédric JUNG <cedced19@gmail.com>
2021-09-27 00:35:18 +0200

    Start coding add_description

---
+++ R/add_description.R
@@ -0,0 +1,6 @@
+
+add_description <- function (stat_units, label) {
+  n <- length(stat_units)
+  hash <- get_hash(n) # wrong model, hash should be linked with stat_unit
+  # TODO: create a function to get hash from stat_unit stat_unit_from_hash ?
+}
---
+++ tests/testthat/test-add_description.R
@@ -0,0 +1,3 @@
+test_that("add_description works", {
+  expect_equal(2 * 2, 4)
+})
