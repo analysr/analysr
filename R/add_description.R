21badfed745cf0f5c2a0de0bac6f841bba840445
Cédric JUNG <cedced19@gmail.com>
2021-09-27 11:50:55 +0200

    Add add_description

--- R/add_description.R
+++ R/add_description.R
@@ -1,6 +1,15 @@
 
+stat_unit_from_hash <- function(stat_units) {
+  subset(analysr_env$stat_units, stat_unit == stat_units)[["hash"]]
+}
+
+
+#' add_description
+#'
+#' @export
 add_description <- function (stat_units, label) {
   n <- length(stat_units)
-  hash <- get_hash(n) # wrong model, hash should be linked with stat_unit
-  # TODO: create a function to get hash from stat_unit stat_unit_from_hash ?
+  hash <- stat_unit_from_hash(stat_units)
+  result <- data.frame(hash = hash, type = rep(label, n), value = TRUE)
+  analysr_env$descriptions <- rbind(analysr_env$descriptions, result)
 }
--- tests/testthat/test-add_description.R
+++ tests/testthat/test-add_description.R
@@ -1,3 +1,13 @@
 test_that("add_description works", {
-  expect_equal(2 * 2, 4)
+  setup_new_env()
+
+  # import model
+  load_env_csv("./csv/add_description/before")
+
+  add_description(c(1, 6), "Fever")
+
+
+  # check model
+  expect_equal(model_state_equal("./csv/add_description/after", analysr_env), TRUE)
+
 })
