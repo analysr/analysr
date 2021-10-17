9b527de76680eb59568657580d5ca6fd8bd453ce
Cédric JUNG <cedced19@gmail.com>
2021-10-17 10:44:40 +0200

    Use underscore when using add_description

--- tests/testthat/test-add_description.R
+++ tests/testthat/test-add_description.R
@@ -8,6 +8,20 @@ test_that("add_description works", {
 
 
   # check model
-  expect_equal(model_state_equal("./csv/add_description/after", analysr_env), TRUE)
+  expect_equal(model_state_equal("./csv/add_description/after1", analysr_env), TRUE)
+
+})
+
+test_that("add_description works with space", {
+  setup_new_env()
+
+  # import model
+  load_env_csv("./csv/add_description/before")
+
+  add_description(c(2, 7), "Fever after surgery")
+
+
+  # check model
+  expect_equal(model_state_equal("./csv/add_description/after2", analysr_env), TRUE)
 
 })
