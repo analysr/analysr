2faa7944f2defa72bf7b2757981500b4816d813d
Cédric JUNG <cedced19@gmail.com>
2021-10-17 13:22:35 +0200

    Add doc

--- R/from_to.R
+++ R/from_to.R
@@ -4,6 +4,13 @@ isDate <- function(x) {
 
 #' from
 #'
+#' @param model A Analysr model instance.
+#' @param date A date object or a string to be converted as a date.
+#' @param date_format_func A function to format date with (not required).
+#' Default: `lubridate::parse_date_time(x, date_format_reg)`
+#' @param date_format_reg A expression to format date with (not required).
+#' Default: `"ymd-HMS"`
+#'
 #' @export
 from <- function(model, date, date_format_func =
                   (function(x) lubridate::parse_date_time(x, date_format_reg)),
@@ -13,6 +20,30 @@ from <- function(model, date, date_format_func =
     model$query$from_date <- date
   } else {
     model$query$from_date <- date_format_func(date)
+  }
+
+  model
+}
+
+
+#' to
+#'
+#' @param model A Analysr model instance.
+#' @param date A date object or a string to be converted as a date.
+#' @param date_format_func A function to format date with (not required).
+#' Default: `lubridate::parse_date_time(x, date_format_reg)`
+#' @param date_format_reg A expression to format date with (not required).
+#' Default: `"ymd-HMS"`
+#'
+#' @export
+to <- function(model, date, date_format_func =
+                  (function(x) lubridate::parse_date_time(x, date_format_reg)),
+                  date_format_reg = "ymd-HMS") {
+
+  if (isDate(date)) {
+    model$query$to_date <- date
+  } else {
+    model$query$to_date <- date_format_func(date)
   }
 
   model
