d3173fb820e9ab33f6463b83ddc8f03060284296
Cédric JUNG <cedced19@gmail.com>
2021-10-17 10:02:52 +0200

    Add at_least usage with before

--- R/before.R
+++ R/before.R
@@ -1,18 +1,34 @@
-is_before <- function(entry, duration, events) {
-  # check if (entry date) =< (event date) and (entry date) >= (event date + duration)
+is_before <- function(entry, duration, events, type) {
+
   found <- FALSE
   events <- subset(events, stat_unit == entry$stat_unit)
 
-  for (i in rownames(events)) {
-    date <- as.numeric(entry$date)
-    start <- as.numeric(events[i,]$date - duration)
-    end <- as.numeric(events[i,]$date)
 
-    if ((date<= end) && (start <= date)) {
-      found <- TRUE
-      break
+  if (type == "at_most") {
+    for (i in rownames(events)) {
+      date <- as.numeric(entry$date)
+      start <- as.numeric(events[i,]$date - duration)
+      end <- as.numeric(events[i,]$date)
+      # check if (entry date) =< (event date)
+      #      and (entry date) >= (event date + duration)
+      if ((date <= end) && (start <= date)) {
+        found <- TRUE
+        break
+      }
     }
   }
+  if (type == "at_least") {
+    for (i in rownames(events)) {
+      date <- as.numeric(entry$date)
+      max <- as.numeric(events[i,]$date - duration)
+      # check if (entry date) =< (event date - duration)
+      if (date <= max) {
+        found <- TRUE
+        break
+      }
+    }
+  }
+
   if (found == TRUE) {
     entry$stat_unit
   } else {
@@ -21,10 +37,10 @@ is_before <- function(entry, duration, events) {
 
 }
 
-is_before_list <- function(entries, duration, events) {
+is_before_list <- function(entries, duration, events, type) {
   stat_units <- c()
   for (i in rownames(entries)) {
-    stat_unit <- is_before(entries[i,], duration, events)
+    stat_unit <- is_before(entries[i,], duration, events, type)
 
     stat_units <- c(stat_units, stat_unit)
   }
@@ -36,15 +52,15 @@ is_before_list <- function(entries, duration, events) {
 #' @export
 before <- function(model, event_tag) {
 
-  if (model$query$duration_type == "at_most"){
 
-    res <- c()
+  res <- c()
 
-    duration <- model$query$duration
-    events <- subset(model$events, tag == event_tag)
+  duration <- model$query$duration
+  events <- subset(model$events, tag == event_tag)
 
-    res <- is_before_list(model$selection, duration, events)
-  }
+  res <- is_before_list(model$selection, duration,
+                        events, model$query$duration_type)
+
 
 
   as.double(res)
--- tests/testthat/test-before.R
+++ tests/testthat/test-before.R
@@ -2,17 +2,17 @@ test_that("before works", {
   setup_new_env()
 
   # import model
-  load_env_csv("./csv/before/before")
+  load_env_csv("./csv/before/before1")
   result <- (
   analysr_env
     %>% observed(Temperature > 38.5)
     %>% at_most(15 * days)
     %>% before("Surgery")
   )
-  query <- list(condition=rlang::expr(Temperature > 38.5),
-                tag="Temperature",
+  query <- list(condition = rlang::expr(Temperature > 38.5),
+                tag = "Temperature",
                 duration_type = "at_most",
-                duration=lubridate::duration(15, "days"))
+                duration = lubridate::duration(15, "days"))
 
   #expected_result
   exp_result <- c(101929077)
@@ -21,13 +21,14 @@ test_that("before works", {
   expect_equal(result, exp_result)
 
   # check model (model should have changed, query also)
-  expect_equal(model_state_equal("./csv/before/after", analysr_env, query), TRUE)
+  expect_equal(model_state_equal("./csv/before/after1", analysr_env, query),
+              TRUE)
 
 
 
 
 })
-test_that("before works", {
+test_that("before works with at_most", {
   setup_new_env()
 
   # import model
@@ -45,8 +46,23 @@ test_that("before works", {
   #check result
   expect_equal(result, exp_result)
 
+})
+test_that("before works with at_least", {
+  setup_new_env()
 
+  # import model
+  load_env_csv("./csv/before/test3")
+  result <- (
+    analysr_env
+    %>% observed(Temperature > 38.5)
+    %>% at_least(15 * days)
+    %>% before("Surgery")
+  )
 
+  #expected_result
+  exp_result <- c(5,1,6)
 
+  #check result
+  expect_equal(result, exp_result)
 
 })
