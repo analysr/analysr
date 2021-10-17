7563a88c71c57c996d5fc466559e67651252e5fc
Cédric JUNG <cedced19@gmail.com>
2021-10-17 13:18:47 +0200

    Add parser

--- R/from_to.R
+++ R/from_to.R
@@ -1,9 +1,19 @@
+isDate <- function(x) {
+  inherits(x, c("Date", "POSIXt"))
+}
+
 #' from
 #'
 #' @export
-from <- function (model, date) {
+from <- function(model, date, date_format_func =
+                  (function(x) lubridate::parse_date_time(x, date_format_reg)),
+                  date_format_reg = "ymd-HMS") {
 
-  model$query$from_date <- date
+  if (isDate(date)) {
+    model$query$from_date <- date
+  } else {
+    model$query$from_date <- date_format_func(date)
+  }
 
   model
 }
--- tests/testthat/test-from_to.R
+++ tests/testthat/test-from_to.R
@@ -1,4 +1,4 @@
-test_that("from works", {
+test_that("from works with date", {
   setup_new_env()
 
   # import model
@@ -8,11 +8,27 @@ test_that("from works", {
   # from
   model <- from(model, as.Date("2006-10-22"))
 
-  # TODO: how to get date ? which parse system ?
-
   query <- list(condition = rlang::expr(Temperature > 38.5),
                 tag = "Temperature",
                 from_date = as.Date("2006-10-22"))
+
+  # check model (model should not have changed, only query has)
+
+  expect_equal(model_state_equal("./csv/from_to/test", model, query), TRUE)
+})
+test_that("from works with date representing date", {
+  setup_new_env()
+
+  # import model
+  load_env_csv("./csv/from_to/test")
+  model <- observed(analysr_env, Temperature > 38.5)
+
+  # from
+  model <- from(model, "2006-10-22", date_format_func = as.Date)
+
+  query <- list(condition = rlang::expr(Temperature > 38.5),
+                tag = "Temperature",
+                from_date = as.Date("2006-10-22", tz = "UTC"))
 
   # check model (model should not have changed, only query has)
 
