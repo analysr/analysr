4a9207b58eabe1d8e9127b814743376633e58384
Cédric JUNG <cedced19@gmail.com>
2021-10-31 13:04:44 +0100

    Code `to` funct

--- tests/testthat/test-from_to.R
+++ tests/testthat/test-from_to.R
@@ -34,3 +34,23 @@ test_that("from works with date representing date", {
 
   expect_equal(model_state_equal("./csv/from_to/test", model, query), TRUE)
 })
+test_that("from to works on a request", {
+  setup_new_env()
+
+  # import model
+  load_env_csv("./csv/from_to/test2")
+  result <- (
+    analysr_env
+    %>% observed(Temperature > 38.5)
+    %>% at_most(15 * days)
+    %>% from("2006/11/09 08:00:00")
+    %>% to("2006/11/11 09:00:00")
+    %>% before("Surgery")
+  )
+  show_env(analysr_env)
+  #expected_result
+  exp_result <- c(7)
+
+  #check result
+  expect_equal(result, exp_result)
+})
