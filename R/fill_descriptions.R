d0946f469f1d102d880bba7ce53a1f9eef69f48d
Cédric JUNG <cedced19@gmail.com>
2021-11-05 12:20:45 +0100

    Fix bug with R CMD Check

--- R/fill_descriptions.R
+++ R/fill_descriptions.R
@@ -7,10 +7,10 @@
 #' @param types a vector of strings (the name of the columns to import)
 #' @param data the data frame you want to extract data from
 #' @param n (optional) length of hash if already calculated
-fill_descriptions <- function(hashes, types, data, n = length(hash)) {
+fill_descriptions <- function(hash, types, data, n = length(hash)) {
 
     prepare_data <- dplyr::select(data,dplyr::all_of(types))
-    prepare_data <- dplyr::mutate(prepare_data, hash = hashes,
+    prepare_data <- dplyr::mutate(prepare_data, hash = hash,
                     .before = types[1])
 
     result <- tidyr::pivot_longer(prepare_data,
