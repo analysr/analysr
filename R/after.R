c7c6abc576b54c23d6a111156ccb309c4f9fb28e
Thomas <thomas.loridan@outlook.com>
2021-11-18 12:32:05 +0100

    Function after

---
+++ R/after.R
@@ -0,0 +1,75 @@
+is_after <- function(entry, duration, selection, type) {
+
+  found <- FALSE
+  selection <- subset(selection, stat_unit == entry$stat_unit)
+
+
+  if (type == "at_most") {
+    for (i in rownames(selection)) {
+      date <- as.numeric(entry$date)
+      start <- as.numeric(selection[i,]$date_obs - duration)
+      end <- as.numeric(selection[i,]$date_obs)
+      # check if (entry date) =< (event date)
+      #      and (entry date) >= (event date + duration)
+      if ((date <= end) && (start <= date)) {
+        found <- TRUE
+        break
+      }
+    }
+  }
+  if (type == "at_least") {
+    for (i in rownames(selection)) {
+      date <- as.numeric(entry$date)
+      max <- as.numeric(selection[i,]$date_obs - duration)
+      # check if (entry date) =< (event date - duration)
+      if (date <= max) {
+        found <- TRUE
+        break
+      }
+    }
+  }
+
+  if (found == TRUE) {
+    entry$stat_unit
+  } else {
+    c()
+  }
+
+}
+
+is_afterlist <- function(entries, duration, selection, type) {
+  stat_units <- c()
+  for (i in rownames(entries)) {
+    stat_unit <- is_after(entries[i,], duration, selection, type)
+
+    stat_units <- c(stat_units, stat_unit)
+  }
+  unique(stat_units)
+}
+
+#' before
+#'
+#' @param model An AnalysR model
+#' @param condition A condition
+#'
+#' @export
+after <- function(model, condition) {
+
+
+  res <- c()
+
+  duration <- model$query$duration
+  condition <- rlang::enexpr(condition)
+  after_selection <- prepare_query(model, condition)
+
+  # TODO: improve periods handeling (here only start date are take into account)
+  # TODO2: what to do with NA values when condition is on descriptions table ?
+
+  res <- is_after_list(model$selection, duration,
+                       before_selection, model$query$duration_type)
+
+
+
+  as.double(res)
+
+}
