f95e95ad2b43bf8fa1914270e73d695a86f82f83
Cédric JUNG <cedced19@gmail.com>
2021-11-22 19:31:59 +0100

    Include all different type of duration

--- R/at_most.R
+++ R/at_most.R
@@ -6,14 +6,33 @@
 #' @export
 at_most <- function(model, e) {
   model$query$duration_type <- "at_most"
+  e <- rlang::enexprs(e)
+  e <- toString(e)
+  e <- tolower(e)
 
-  e <- rlang::enexpr(e)
+  e <- stringr::str_replace_all(e, "years", "31557600")
+  e <- stringr::str_replace_all(e, "year", "31557600")
 
-  n <- e[[2]]
-  unit <- rlang::as_string(e[[3]])[1]
-  # TODO: create an aux fnct that can understand 1*days+2*hours
+  e <- stringr::str_replace_all(e, "months", "2678400")
+  e <- stringr::str_replace_all(e, "month", "2678400")
 
-  model$query$duration <- lubridate::duration(n, unit)
+  e <- stringr::str_replace_all(e, "weeks", "604800")
+  e <- stringr::str_replace_all(e, "week", "604800")
+
+  e <- stringr::str_replace_all(e, "days", "86400")
+  e <- stringr::str_replace_all(e, "day", "86400")
+
+  e <-stringr::str_replace_all(e,  "hours", "3600")
+  e <-stringr::str_replace_all(e,  "hour", "3600")
+
+  e <-stringr::str_replace_all(e,  "minutes", "60")
+  e <-stringr::str_replace_all(e,  "minute", "60")
+
+  e <-stringr::str_replace_all(e,  "seconds", "1")
+  e <-stringr::str_replace_all(e,  "second", "1")
+
+  durationSeconds <- eval(str2expression(e))
+  model$query$duration <- lubridate::duration(durationSeconds, "seconds")
 
   model
 }
--- tests/testthat/test-at_most.R
+++ tests/testthat/test-at_most.R
@@ -1,4 +1,4 @@
-test_that("at_most works", {
+test_that("at_most works for 15 days", {
   setup_new_env()
 
   # import model
@@ -16,6 +16,26 @@ test_that("at_most works", {
                 duration = lubridate::duration(15, "days"))
 
   # check model (model should not have changed, only query has)
-
   expect_equal(model_state_equal("./csv/at_most/model", model, query), TRUE)
 })
+test_that("at_most works for compelex date", {
+  setup_new_env()
+
+  # import model
+  load_env_csv("./csv/at_most/model")
+  model <- observed(analysr_env, Temperature > 38.5)
+
+  # at_most
+  model <- at_most(model, 15*days+12*(1*week+1*second))
+
+
+
+  query <- list(condition = rlang::expr(Temperature > 38.5),
+                tag = "Temperature",
+                duration_type = "at_most",
+                duration = lubridate::duration(15+7*12, "days") + lubridate::duration(12, "seconds"))
+
+  # check model (model should not have changed, only query has)
+  expect_equal(model_state_equal("./csv/at_most/model", model, query), TRUE)
+})
+
