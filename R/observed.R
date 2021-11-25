243dcdb2798436a04429aa782d44df2c92da6a78
Cédric JUNG <cedced19@gmail.com>
2021-11-25 08:26:39 +0100

    Fix bug with hashs on selection

--- R/observed.R
+++ R/observed.R
@@ -13,8 +13,8 @@ stat_unit_from_hash <- function(hashs) {
 hash_from_stat_unit <- function(model, stat_units) {
   result <- c()
   if (length(stat_units) != 0) {
+  for (j in stat_units) {
     for (i in rownames(model$stat_units)) {
-      for (j in stat_units) {
         if (model$stat_units[i,]$stat_unit == j) {
           result <- c(result, model$stat_units[i,]$hash)
         }
