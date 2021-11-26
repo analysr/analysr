18d5a561b293bd6dfb6bbfb3478b33b73cc27e4b
Cédric JUNG <cedced19@gmail.com>
2021-11-26 20:03:22 +0100

    Fix test for instide_outside

--- tests/testthat/test-inside_outside.R
+++ tests/testthat/test-inside_outside.R
@@ -2,8 +2,8 @@ test_that("inside works on a request", {
   setup_new_env()
 
   # import model
-  load_env_csv("./csv/inside_outside/test")
-  result <- (
+  load_env_csv("./csv/inside_outside/test1")
+  model <- (
     analysr_env
     %>% observed(Temperature > 38.5)
     %>% at_most(15 * days)
@@ -11,18 +11,21 @@ test_that("inside works on a request", {
     %>% before("Surgery")
   )
 
-  #expected_result
-  exp_result <- c(7)
+  query <- list(condition = rlang::expr(Temperature > 38.5),
+                tag = "Temperature",
+                duration_type = "at_most",
+                duration = lubridate::duration(15, "days"))
 
   #check result
-  expect_equal(result, exp_result)
+  expect_equal(model_state_equal("./csv/inside_outside/test1", model, query),
+               TRUE)
 })
 test_that("outside works on a request", {
   setup_new_env()
 
   # import model
   load_env_csv("./csv/inside_outside/test2")
-  result <- (
+  model <- (
     analysr_env
     %>% observed(Temperature > 38.5)
     %>% at_most(15 * days)
@@ -30,9 +33,16 @@ test_that("outside works on a request", {
     %>% before("Surgery")
   )
 
-  #expected_result
-  exp_result <- c(1)
+  query <- list(condition = rlang::expr(Temperature > 38.5),
+                tag = "Temperature",
+                duration_type = "at_most",
+                duration = lubridate::duration(15, "days"))
+
 
   #check result
-  expect_equal(result, exp_result)
+  expect_equal(model_state_equal("./csv/inside_outside/test2", model, query),
+               TRUE)
+
+  #expected_result
+  exp_result <- c(1)
 })
