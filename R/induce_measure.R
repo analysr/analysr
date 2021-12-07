45f11dcf8c7221d14c2ffd1a9d1811dfabc6c3b5
Cédric JUNG <cedced19@gmail.com>
2021-12-07 19:49:49 +0100

    List tabs to be used in measures table

---
+++ R/induce_measure.R
@@ -0,0 +1,41 @@
+
+convert <- function(ee) {
+  tried <- try(ee <- rlang::as_string(ee), silent = TRUE)
+  if(!inherits(tried, "try-error")) {
+    ee
+  }
+}
+
+get_AST <- function(ee) purrr::map_if(as.vector(ee), is.call, get_AST, .else = convert)
+
+get_tags <- function(calcul) {
+  len <- length(calcul)
+  if (len > 1) {
+      vect <- c()
+      for (i in 2:len) {
+        if (typeof(calcul[i][[1]]) == "character") {
+          vect <- c(vect, calcul[i][[1]])
+        } else {
+          vect <- c(vect, get_tags(calcul[i][[1]]))
+        }
+      }
+      return(vect)
+  }
+  c()
+}
+
+
+#' induce_measure
+#'
+#' @param model An AnalysR env
+#' @param tag_to_create Label to write in measure table
+#' @param calcul A function containing at least a measures tag
+#' @param tag_ref A tag that is considered to iter on measures table
+#'
+#' @export
+induce_measure <- function(model, tag_to_create, calcul, tag_ref) {
+  calcul <- rlang::enexpr(calcul)
+  calcul_as_string <- toString(calcul)
+  calcul <- get_AST(calcul)
+  tags <- get_tags(calcul)
+}
---
+++ tests/testthat/test-induce_measure.R
@@ -0,0 +1,13 @@
+test_that("induce_measure works", {
+
+  setup_new_env()
+  #(lobstr::ast(Length*4+add(Weight*2,8)))
+  #print(induce_measure(analysr_env,"BMI",Length*4+add(Weight*2,8)))
+  #print(induce_measure(analysr_env,"BMI",Length+add(Weight*divide(7),8)))
+  #print(induce_measure(analysr_env,"BMI",Length+Vectsd))
+  #print(induce_measure(analysr_env,"BMI",add()))
+  #print(induce_measure(analysr_env,"BMI",add(7)))
+  #print(lobstr::ast(add(8,9,Vecrt)))
+  #print(induce_measure(analysr_env,"BMI",add(8,9,divid(Vecrt,d(ffd),15))))
+  #print(induce_measure(analysr_env,"BMI",add(8,9,Vectsd)))
+})
