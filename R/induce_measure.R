cec2eb4900254113b501aaf965999c599dc401ac
Cédric JUNG <cedced19@gmail.com>
2021-12-07 21:48:39 +0100

    Add induce_measure

--- R/induce_measure.R
+++ R/induce_measure.R
@@ -26,8 +26,10 @@ get_tags <- function(calcul) {
 
 
 replace_values <- function(string, tags_arr, values_arr) {
+  string <- stringr::str_dup(string, 1)
   for (i in 1:length(tags_arr)) {
-    string <- stringr::str_replace_all(string, tags_arr[i], toString(values_arr[i]))
+    string <- stringr::str_replace_all(string, tags_arr[i],
+                                               toString(values_arr[i]))
   }
   string
 }
@@ -42,7 +44,52 @@ replace_values <- function(string, tags_arr, values_arr) {
 #' @export
 induce_measure <- function(model, tag_to_create, calcul, tag_ref) {
   calcul <- rlang::enexpr(calcul)
-  calcul_as_string <- toString(calcul)
+  calcul_as_string <- deparse(calcul)
   calcul <- get_AST(calcul)
   tags <- get_tags(calcul)
+
+  if (length(tags) == 0) {
+    stop("No tag found")
+  }
+
+  if (missing(tag_ref)) {
+    tag_ref <- tags[1]
+  }
+
+  tmp <- dplyr::filter(model$measures, tag == tag_ref)
+  for(i in 1:nrow(tmp)) {
+    entry <- tmp[i, ]
+    # Search other data
+    other_data <- dplyr::filter(model$measures, tag %in% tags,
+                                          tag != tag_ref,
+                                          stat_unit == entry$stat_unit,
+                                          date < entry$date)
+    # Order data by time desc order
+    other_data <- other_data[rev(order(other_data$date)), ]
+    other_data <- dplyr::slice(dplyr::group_by(other_data, tag),
+                                which.max(date))
+
+    # TODO: add more flexibility (maybe other data can be better after the
+    # tag_ref)
+
+    # Evaluate
+    tags_arr <- c(entry$tag, other_data$tag)
+    values_arr <- c(entry$value, other_data$value)
+
+    to_eval <- replace_values(calcul_as_string, tags_arr, values_arr)
+
+    value <- eval(str2expression(to_eval))
+    # TODO: add way to round this value
+
+    new_entry <- tibble::tibble(hash = get_hash(),
+                                    stat_unit = entry$stat_unit,
+                                    date = entry$date,
+                                    tag  = tag_to_create,
+                                    value = value,
+                                    status = NA)
+
+    model$measures <- rbind(model$measures, new_entry)
+  }
+
+  model
 }
