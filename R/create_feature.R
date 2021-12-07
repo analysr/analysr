b6ab901952b9f227a07b773bb4363709626a6fff
Elisa JF <elisajf2001@gmail.com>
2021-12-07 15:47:48 +0100

    Add CREATE_FEATURE with 1 test

---
+++ R/create_feature.R
@@ -0,0 +1,42 @@
+#' create_feature
+#'
+#' @param model An AnalysR model
+#' @param tag_to_create Label to write in description table
+#' @param wanted_tag The name of the measures we want to work on
+#' @param start A date marking the beginning of the studied time period.
+#' @param end A date marking the end of the studied time period.
+#' @param aggregation_method A function to aggregate data.
+#' Default: mean_aggregate
+#'
+#' @export
+
+create_feature <-
+  function(model,
+           tag_to_create,
+           wanted_tag,
+           start,
+           end,
+           aggregation_method = mean_aggregate){
+
+    data <- subset(model$measures, tag == wanted_tag)
+    data <- subset (data, date <= end)
+    data <- subset (data, date >= start)
+
+    to_add <- tibble::tibble(stat_unit = integer(0), new = numeric(0))
+
+    stat <- unique(model$measures$stat_unit)
+    for (s in stat){
+      sample <- subset(data, stat_unit == s)
+      if ((n <- nrow(data)) != 0) {
+        temp <- tibble::tibble(stat_unit = c(s),
+                               new = aggregation_method(sample$value))
+        to_add <- rbind(to_add, temp)
+      }
+    }
+
+  colnames(to_add) <- c("stat_unit", tag_to_create)
+  model$stat_units <- dplyr::left_join(model$stat_units, to_add, by = "stat_unit")
+
+  model
+  }
+
---
+++ tests/testthat/test-create_feature.R
@@ -0,0 +1,16 @@
+test_that("create_feature works", {
+  setup_new_env()
+
+  # import measures
+  load_env_csv("./csv/create_feature/before1")
+
+  # restrict
+  model <- create_feature(analysr_env,
+                          "Temp_mooy_2006",
+                          "Temperature",
+                          lubridate::parse_date_time("2006/01/01 01:00:00", "ymd-HMS"),
+                          lubridate::parse_date_time("2006/12/31 23:59:00", "ymd-HMS"))
+
+  # check model
+  expect_equal(model_state_equal("./csv/create_feature/after1", model), TRUE)
+})
