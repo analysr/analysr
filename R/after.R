a07d7ff66a24f55d98a4594d5580d91ab5a020ef
Cédric JUNG <cedced19@gmail.com>
2021-12-21 16:40:36 +0100

    Split (do not subset each time)

--- R/after.R
+++ R/after.R
@@ -1,18 +1,23 @@
-is_after <- function(entry, duration, selection, type) {
+is_after <- function(entry, duration, splitted, type) {
   # here selection refer to the selection done by condition on after
   found <- FALSE
-  selection <- subset(selection, stat_unit == entry$stat_unit)
+  selection <- splitted[[toString(entry$stat_unit)]]
   temporal_hash <- -1
+
+  date <- as.numeric(entry$date_obs)
 
   if (type == "at_most") {
     for (i in rownames(selection)) {
-      date <- as.numeric(entry$date_obs)
-      if (is.na(entry$date_obs_end)){#means it's not a period
+      if (is.na(entry$date_obs_end)){
+        #means it's not a period
         start <- as.numeric(selection[i,]$date_obs)
-        end <- as.numeric(selection[i,]$date_obs + duration)}
-      else {#means it's a period, we take the date of the end
+        end <- as.numeric(selection[i,]$date_obs + duration)
+      } else {
+        #means it's a period, we take the date of the end
         start <- as.numeric(selection[i,]$date_obs_end)
-        end <- as.numeric(selection[i,]$date_obs_end + duration)}
+        end <- as.numeric(selection[i,]$date_obs_end + duration)
+      }
+
       # check if (selection date) <= (entry date) =< (selection date + duration)
       if ((start <= date) && (date <= end)) {
         found <- TRUE
@@ -23,18 +28,15 @@ is_after <- function(entry, duration, selection, type) {
   }
 
 
-
-      else {#means it's a period, we take the date of the end
-        date <- as.numeric(entry$date_obs_end)}
-
-
   if (type == "at_least") {
     for (i in rownames(selection)) {
-      date <- as.numeric(entry$date_obs)
-      if (is.na(entry$date_obs_end)){#means it's not a period
-        min <- as.numeric(selection[i,]$date_obs + duration)}
-      else {#means it's a period, we take the date of the end
-        min <- as.numeric(selection[i,]$date_obs_end + duration)}
+      if (is.na(entry$date_obs_end)){
+        #means it's not a period
+        min <- as.numeric(selection[i,]$date_obs + duration)
+      } else {
+        #means it's a period, we take the date of the end
+        min <- as.numeric(selection[i,]$date_obs_end + duration)
+      }
       # check if (entry date) >= (event date + duration)
       if (date >= min) {
         found <- TRUE
@@ -50,8 +52,9 @@ is_after <- function(entry, duration, selection, type) {
 is_after_list <- function(entries, duration, selection, type) {
   # here selection refer to the selection done by condition on after
   res <- tibble::tibble()
+  splitted <- split(selection, selection$stat_unit)
   for (i in rownames(entries)) {
-    tmp <- is_after(entries[i,], duration, selection, type)
+    tmp <- is_after(entries[i,], duration, splitted, type)
 
     if (tmp$found) {
       new_entry <- dplyr::bind_cols(entries[i,], temporal_hash = tmp$temporal_hash)
@@ -76,8 +79,6 @@ after <- function(model, condition) {
   duration <- model$query$duration
   condition <- rlang::enexpr(condition)
   after_selection <- prepare_query(model, condition)
-
-  # TODO: improve periods handling (here only start date are take into account)
 
   res <- is_after_list(model$selection, duration,
                         after_selection, model$query$duration_type)
--- R/before.R
+++ R/before.R
@@ -1,7 +1,7 @@
-is_before <- function(entry, duration, selection, type) {
+is_before <- function(entry, duration, splitted, type) {
   # here selection refer to the selection done by condition on before
   found <- FALSE
-
+  selection <- splitted[[toString(entry$stat_unit)]]
   temporal_hash <- -1
 
   if (is.na(entry$date_obs_end)){
@@ -43,17 +43,14 @@ is_before <- function(entry, duration, selection, type) {
 is_before_list <- function(entries, duration, selection, type) {
   # here selection refer to the selection done by condition on before
   res <- tibble::tibble()
-  splitted <- dplyr::group_split(entries, stat_unit)
-  for (tbl in splitted) {
-    selection_by_stat_units <- dplyr::filter(selection, stat_unit == tbl$stat_unit[1])
-    for (i in rownames(tbl)) {
-      tmp <- is_before(tbl[i,], duration, selection_by_stat_units, type)
+  splitted <- split(selection, selection$stat_unit)
+  for (i in rownames(entries)) {
+      tmp <- is_before(entries[i,], duration, splitted, type)
 
       if (tmp$found) {
-        new_entry <- dplyr::bind_cols(tbl[i,], temporal_hash = tmp$temporal_hash)
+        new_entry <- dplyr::bind_cols(entries[i,], temporal_hash = tmp$temporal_hash)
         res <- dplyr::bind_rows(res, new_entry)
       }
-    }
   }
   res
 }
@@ -74,10 +71,8 @@ before <- function(model, condition) {
   condition <- rlang::enexpr(condition)
   before_selection <- prepare_query(model, condition)
 
-  tictoc::tic("is_before_list")
   res <- is_before_list(model$selection, duration,
                         before_selection, model$query$duration_type)
-  tictoc::toc()
 
   model$selection <- res
   model
